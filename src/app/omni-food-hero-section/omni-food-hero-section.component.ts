f55d3a12043b3e84b6fdee9e7da17d71ed06738b
vinayak teradali <vteradali5@gmail.com>
2025-02-08 20:25:36 +0530

    delivared-images

--- src/app/omni-food-hero-section/omni-food-hero-section.component.ts
+++ src/app/omni-food-hero-section/omni-food-hero-section.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-omni-food-hero-section',
   templateUrl: './omni-food-hero-section.component.html',
-  styleUrls: ['./omni-food-hero-section.component.css']
+  styleUrls: ['./omni-food-hero-section.component.css'],
 })
-export class OmniFoodHeroSectionComponent {
+export class OmniFoodHeroSectionComponent implements OnInit {
+  customerData: string[] = [];
 
+  ngOnInit(): void {
+    this.customerData = [
+      '../../assets/customers/customer-1.jpg',
+      '../../assets/customers/customer-2.jpg',
+      '../../assets/customers/customer-3.jpg',
+      '../../assets/customers/customer-4.jpg',
+      '../../assets/customers/customer-5.jpg',
+      '../../assets/customers/customer-6.jpg',
+      '../../assets/customers/ben.jpg',
+      '../../assets/customers/dave.jpg',
+    ];
+  }
 }
